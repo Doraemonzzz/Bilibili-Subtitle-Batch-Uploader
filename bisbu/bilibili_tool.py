8f042e7b0871eba89669c9ab9b17249bdb22e70d
seeker0472 <gmx472@qq.com>
2024-03-29 21:50:49 +0800

    在GET请求的header部分加入了User-Agent修复了接口412错误

--- bisbu/bilibili_tool.py
+++ bisbu/bilibili_tool.py
@@ -40,7 +40,10 @@ def get_bvdata(bvid, url=BVDATA_API):
     获得bv数据
     """
     url += f'?bvid={bvid}'
-    response = requests.request("GET", url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.request("GET", url, headers=headers)
 
     return json.loads(response.text)
 
